c984ee53b02a08af874df486b6f8e3839a705750
Alex Yagni <near1tfeex@gmail.com>
2024-10-25 21:45:31 +0500

    rolling back the GitHubApiClient.h file

--- Hub-activity/GitHubApiClient.h
+++ Hub-activity/GitHubApiClient.h
@@ -28,39 +28,13 @@ public:
 		WSACleanup();
 	}
 
-	std::string SendGetRequest(std::string_view endpoint) {
-		SOCKET clientSocket{ CreateSocket() };
-
-		addrinfo* info{ CreateAddrinfo(clientSocket, "api.github.com", "443") };
-
-		Connect(clientSocket, info);
-
-		std::string request{ GetHTTPRequest(endpoint) };
-
-		Send(clientSocket, info, request);
-		
-		DWORD timeout = 30000; // Timeout in milliseconds (30 seconds)
-		SetTimeout(clientSocket, info, timeout);
-
-		std::string response{ ReadDataFromSocket(clientSocket, info) };
-
-		closesocket(clientSocket);
-		freeaddrinfo(info);
-
-		return response;
-	}
-
-	SOCKET CreateSocket() {
-		SOCKET clientSocket{ socket(AF_INET, SOCK_STREAM, 0) };
-		if (clientSocket == INVALID_SOCKET) {
-			closesocket(clientSocket);
+	std::string SendGetRequest(const std::string& endpoint) {
+		SOCKET serverSocket{ socket(AF_INET, SOCK_STREAM, 0) };
+		if (serverSocket == INVALID_SOCKET) {
+			closesocket(serverSocket);
 			throw std::runtime_error{ "Error: Initialization socket # " + WSAGetLastError() + '.' + '\n' };
 		}
 
-		return clientSocket;
-	}
-
-	addrinfo* CreateAddrinfo(SOCKET clientSocket, std::string_view hostName, std::string_view serviceName) {
 		addrinfo hints;
 		ZeroMemory(&hints, sizeof(hints));
 		hints.ai_family = AF_INET;
@@ -68,74 +42,62 @@ public:
 		hints.ai_protocol = IPPROTO_TCP;
 
 		addrinfo* result = nullptr;
-		if (getaddrinfo(hostName.data(), serviceName.data(), &hints, &result) != 0) { 
-			closesocket(clientSocket);
+		if (getaddrinfo("api.github.com", "80", &hints, &result) != 0) { // Port 443 for HTTPS
+			closesocket(serverSocket);
 			throw std::runtime_error{ "Error resolving hostname: " + WSAGetLastError() + '\n' };
 		}
 
-		return result;
-	}
-
-	void Connect(SOCKET clientSocket, addrinfo* info) {
-		int isConnect{ connect(clientSocket, info -> ai_addr, (int)info -> ai_addrlen) };
+		int isConnect{ connect(serverSocket, result->ai_addr, (int)result->ai_addrlen) };
 		if (isConnect == SOCKET_ERROR) {
-			closesocket(clientSocket);
-			freeaddrinfo(info);
+			closesocket(serverSocket);
+			freeaddrinfo(result);
 			throw std::runtime_error{ "Error connecting to server: " + WSAGetLastError() + '\n' };
 		}
-	}
 
-	std::string GetHTTPRequest(std::string_view endpoint) {
-		std::string request;
+		std::string request = "GET " +
+			endpoint +
+			" HTTP/1.1\r\n" +
+			"Host: api.github.com\r\n" +
+			"Accept: application/vnd.github+json\r\n" +
+			"User-Agent: Hub-activity\r\n" +
+			"Connection: keep-alive\r\n\r\n";
 
-		request.append("GET ");
-		request.append(endpoint.data());
-		request.append(" HTTP/1.1\r\n");
-		request.append("Host: api.github.com\r\n");
-		request.append("Accept: application/vnd.github+json\r\n");
-		request.append("User-Agent: Hub-activity\r\n");
-		request.append("Connection: keep-alive\r\n\r\n");
-
-		return request;
-	}
-
-	void Send(SOCKET clientSocket, addrinfo* info, std::string_view request) {
-		int size{ static_cast<int>(request.size()) };
-		int bytesSent = send(clientSocket, request.data(), size, 0);
+		int bytesSent = send(serverSocket, request.c_str(), static_cast<int>(request.size()), 0);
 		if (bytesSent == SOCKET_ERROR) {
-			closesocket(clientSocket);
-			freeaddrinfo(info);
+			closesocket(serverSocket);
+			freeaddrinfo(result);
 			throw std::runtime_error{ "Error sending request: " + WSAGetLastError() + '\n' };
 		}
-	}
 
-	void SetTimeout(SOCKET clientSocket, addrinfo* info, DWORD timeout) {
-		int ans{ setsockopt(clientSocket, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout)) };
+		DWORD timeout = 30000; // Timeout in milliseconds (30 seconds)
+		int ans{ setsockopt(serverSocket, SOL_SOCKET, SO_RCVTIMEO, (char*)&timeout, sizeof(timeout)) };
 		if (ans == SOCKET_ERROR) {
-			closesocket(clientSocket);
-			freeaddrinfo(info);
+			closesocket(serverSocket);
+			freeaddrinfo(result);
 			throw std::runtime_error{ "Error setting receive timeout: " + WSAGetLastError() + '\n' };
 		}
-	}
 
-	std::string ReadDataFromSocket(SOCKET clientSocket, addrinfo* info) {
 		char buffer[1024];
 		std::string response;
 		int bytesReceived;
 		while (true) {
-			bytesReceived = recv(clientSocket, buffer, sizeof buffer, 0);
+			bytesReceived = recv(serverSocket, buffer, sizeof buffer, 0);
 			if (bytesReceived == SOCKET_ERROR) {
-				closesocket(clientSocket);
-				freeaddrinfo(info);
+				closesocket(serverSocket);
+				freeaddrinfo(result);
 				throw std::runtime_error{ "Error sending request: " + WSAGetLastError() + '\n' };
 			}
 			else if (bytesReceived == 0) {
+				std::cout << "Connection closed by server.\n";
 				break;
 			}
 			else {
 				response.append(buffer, bytesReceived);
 			}
 		}
+
+		closesocket(serverSocket);
+		freeaddrinfo(result);
 
 		return response;
 	}
